cb9449ce41fa2b089a031254af7b9c36d8669229
Jonathan Marking <jonathanmarking@Jonathans-MacBook-Pro.local>
2019-04-07 13:41:38 -0500

    - Added JavaScript tests for all specific use cases and normal cases. - Refactored JavaScript update_quality method. - Fixed/added Conjured item functionality. - Updated gitignore to ignore MAC DS_Store files.

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,122 @@
-describe("Gilded Rose", function() {
+describe("Gilded Rose", function () {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it('should not change sell by or quality as legendary Sulfuras item', function () {
+    items = [new Item('Sulfuras, Hand of Ragnaros', 5, 120)];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual('Sulfuras, Hand of Ragnaros');
+    expect(items[0].sell_in).toEqual(5);
+    expect(items[0].quality).toEqual(120);
   });
 
+  it('should decrease quality by 1 for normal item', function () {
+    items = [new Item('Elixir of the Mongoose', 1, 50)];
+    update_quality();
+    expect(items[0].name).toEqual('Elixir of the Mongoose');
+    expect(items[0].sell_in).toEqual(0);
+    expect(items[0].quality).toEqual(49);
+  });
+
+  it('should decrease quality by 2 for normal item', function () {
+    items = [new Item('+5 Dexterity Vest', 0, 50)];
+    update_quality();
+    expect(items[0].name).toEqual('+5 Dexterity Vest');
+    expect(items[0].sell_in).toEqual(-1);
+    expect(items[0].quality).toEqual(48);
+  });
+
+  it('should not decrease quality below 0 for normal item', function () {
+    items = [new Item('Elixir of the Mongoose', 4, 0)];
+    update_quality();
+    expect(items[0].name).toEqual('Elixir of the Mongoose');
+    expect(items[0].sell_in).toEqual(3);
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it('should increase quality by 1 for Aged Brie item', function () {
+    items = [new Item('Aged Brie', 5, 14)];
+    update_quality();
+    expect(items[0].name).toEqual('Aged Brie');
+    expect(items[0].sell_in).toEqual(4);
+    expect(items[0].quality).toEqual(15);
+  });
+
+  it('should increase quality by 2 for Aged Brie item', function () {
+    items = [new Item('Aged Brie', -1, 14)];
+    update_quality();
+    expect(items[0].name).toEqual('Aged Brie');
+    expect(items[0].sell_in).toEqual(-2);
+    expect(items[0].quality).toEqual(16);
+  });
+
+  it('should not increase quality above 50 for Aged Brie item', function () {
+    items = [new Item('Aged Brie', -1, 49)];
+    update_quality();
+    expect(items[0].name).toEqual('Aged Brie');
+    expect(items[0].sell_in).toEqual(-2);
+    expect(items[0].quality).toEqual(50);
+  });
+
+  it('should increase quality by 1 for Backstage pass', function () {
+    items = [new Item('Backstage passes to a TAFKAL80ETC concert', 12, 13)];
+    update_quality();
+    expect(items[0].name).toEqual('Backstage passes to a TAFKAL80ETC concert');
+    expect(items[0].sell_in).toEqual(11);
+    expect(items[0].quality).toEqual(14);
+  });
+
+  it('should increase quality by 2 for Backstage pass', function () {
+    items = [new Item('Backstage passes to a TAFKAL80ETC concert', 11, 13)];
+    update_quality();
+    expect(items[0].name).toEqual('Backstage passes to a TAFKAL80ETC concert');
+    expect(items[0].sell_in).toEqual(10);
+    expect(items[0].quality).toEqual(15);
+  });
+
+  it('should increase quality by 3 for Backstage pass', function () {
+    items = [new Item('Backstage passes to a TAFKAL80ETC concert', 6, 13)];
+    update_quality();
+    expect(items[0].name).toEqual('Backstage passes to a TAFKAL80ETC concert');
+    expect(items[0].sell_in).toEqual(5);
+    expect(items[0].quality).toEqual(16);
+  });
+
+  it('should not increment quality above 50 for Backstage pass', function () {
+    items = [new Item('Backstage passes to a TAFKAL80ETC concert', 6, 49)];
+    update_quality();
+    expect(items[0].name).toEqual('Backstage passes to a TAFKAL80ETC concert');
+    expect(items[0].sell_in).toEqual(5);
+    expect(items[0].quality).toEqual(50);
+  });
+
+  it('should set quality to 0 for Backstage pass', function () {
+    items = [new Item('Backstage passes to a TAFKAL80ETC concert', 0, 49)];
+    update_quality();
+    expect(items[0].name).toEqual('Backstage passes to a TAFKAL80ETC concert');
+    expect(items[0].sell_in).toEqual(-1);
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it('should decrease quality by 4 for Conjured item', function () {
+    items = [new Item('Conjured Sparkling Water', 0, 49)];
+    update_quality();
+    expect(items[0].name).toEqual('Conjured Sparkling Water');
+    expect(items[0].sell_in).toEqual(-1);
+    expect(items[0].quality).toEqual(45);
+  });
+
+  it('should decrease quality by 2 for Conjured item', function () {
+    items = [new Item('Conjured Mana Biscuit', 1, 49)];
+    update_quality();
+    expect(items[0].name).toEqual('Conjured Mana Biscuit');
+    expect(items[0].sell_in).toEqual(0);
+    expect(items[0].quality).toEqual(47);
+  });
+
+  it('should decrease quality to 0 for Conjured item', function () {
+    items = [new Item('Conjured Mana Strudel', 0, 3)];
+    update_quality();
+    expect(items[0].name).toEqual('Conjured Mana Strudel');
+    expect(items[0].sell_in).toEqual(-1);
+    expect(items[0].quality).toEqual(0);
+  });
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -8,48 +8,60 @@ var items = []
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
+    if (items[i].name !== 'Sulfuras, Hand of Ragnaros') {
+      items[i].sell_in -= 1;
+
+      if (items[i].name === 'Backstage passes to a TAFKAL80ETC concert')
+        BackstagePass(i);
+      else if (items[i].name === 'Aged Brie')
+        IncreaseQuality(i, 1);
+      else if (items[i].name.includes('Conjured'))
+        DecreaseQuality(i, 2);
+      else
+        DecreaseQuality(i, 1);
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
-    }
+  }
+}
+
+function BackstagePass(index) {
+  let sellIn = items[index].sell_in;
+
+  if (sellIn < 0)
+    items[index].quality = 0;
+  else {
+    let qualityIncrement = 1;
+
+    if (sellIn <= 10 && sellIn > 5)
+      qualityIncrement = 2;
+    if (sellIn <= 5 && sellIn >= 0)
+      qualityIncrement = 3;
+
+    IncreaseQuality(index, qualityIncrement);
+  }
+}
+
+function IncreaseQuality(index, qualityIncrement) {
+  if (items[index].quality < 50) {
+    // Would like to ask Leeroy if this is correct.  Leeroy implemented this originally, but
+    // the specs do not say anything about quality INCREASING twice as fast for past sell_in
+    if (items[index].sell_in < 0)
+      qualityIncrement *= 2;
+
+    if (items[index].quality + qualityIncrement > 50)
+      items[index].quality = 50;
+    else
+      items[index].quality += qualityIncrement;
+  }
+}
+
+function DecreaseQuality(index, qualityDecrement) {
+  if (items[index].quality > 0) {
+    if (items[index].sell_in < 0)
+      qualityDecrement *= 2;
+
+    if (items[index].quality - qualityDecrement < 0)
+      items[index].quality = 0;
+    else
+      items[index].quality -= qualityDecrement;
   }
 }
